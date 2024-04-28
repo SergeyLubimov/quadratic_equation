aecb8059305c01d5ad04128f5d83207cd640cd1f
SergeyLubimov <logoskaizar@yandex.ru>
2024-04-28 20:41:52 +0700

    Create a module test.

--- test_quadratic_equation.c
+++ test_quadratic_equation.c
@@ -1,10 +1,33 @@
 #include "quadratic_equation.h"
+#include <stdbool.h>
+
+void output_solution(double a, double b, double c, double expected_x1, double expected_x2, bool is_solution)
+{
+	double *x;             
+	x = solve_equation(a, b, c); 
+	
+	printf("\n(a = %f, b = %f, d = %f)\nResult:", a, b, c);
+	if(x == 0) printf("\nThere are no solutions.");
+	else printf("\nx1 = %f,\nx2 = %f.", x[0], x[1]); 
+	
+	printf("\nExpected result:");
+	if(is_solution == false) printf("\nThere are no solutions.\n");
+	else printf("\nx1 = %f,\nx2 = %f.\n", expected_x1, expected_x2); 
+}
 
 int main()                 
 {         
-	double *x;             
-	x = solve_equation(1, -4, 4); 
-	if(x == 0) printf("\nThere are no solutions.\n");
-	else printf("\nx1 = %f,\nx2 = %f.\n", x[0], x[1]); 
+	int number_of_tests = 3;
+	double test[3][6] =
+	{ 
+		{1, -4, 4, 2, 2, 1}, 
+		{1, 3, -4, 1, -4, 1}, 
+		{1, -5, 9, 0, 0, 0}
+	};
+	
+	for(int i = 0; i < number_of_tests; i++)
+		output_solution(test[i][0], test[i][1], test[i][2], 
+				test[i][3], test[i][4], test[i][5]);
+	
 	return 0;                   
 }     
