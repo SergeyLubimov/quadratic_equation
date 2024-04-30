611ce1d45f454f0f9356990f4ca670b79fc7cd1e
SergeyLubimov <logoskaizar@yandex.ru>
2024-04-30 10:27:10 +0700

    Fix testing for the VIS version

--- test_quadratic_equation.c
+++ test_quadratic_equation.c
@@ -8,73 +8,74 @@
 void 
 output_solution
 ( 
-  double x1, double x2, bool is_solution_found,
+  double r_x1, double i_x1, double r_x2, double i_x2,
   double a, double b, double c, 
-  double expected_x1, double expected_x2, bool is_solution
+  double expected_r_x1, double expected_i_x1, double expected_r_x2, double expected_i_x2
 )
 {	
 	printf("\n\t(a = %f, b = %f, d = %f)\n\tResult:", a, b, c);
-	if(is_solution_found) printf("\n\tx1 = %f,\n\tx2 = %f.", x1, x2);
-	else  printf("\n\tThere are no solutions.");
-	
+	printf("\n\tx1 = %f + %fi,\n\tx2 = %f + %fi.", r_x1, i_x1, r_x2, i_x2);
+		
 	printf("\n\tExpected result:");
-	if(is_solution) printf("\n\tx1 = %f,\n\tx2 = %f.\n\n", expected_x1, expected_x2); 
-	else printf("\n\tThere are no solutions.\n");
+	printf("\n\tx1 = %f + %fi,\n\tx2 = %f + %fi.\n\n", expected_r_x1, expected_i_x1,
+							   expected_r_x2, expected_i_x2); 
 }
 
 
 bool 
 check_the_solution
 ( 
-  double x1, double x2, bool is_solution_found, 
-  double expected_x1, double expected_x2, bool is_solution
+  double r_x1, double i_x1, double r_x2, double i_x2, 
+  double expected_r_x1, double expected_i_x1, double expected_r_x2, double expected_i_x2
 )
-{
-	if(is_solution_found == is_solution)
-	{
-		if(is_solution_found == false || (x1 == expected_x1 && x2 == expected_x2)) 
-			return true;
-	}
-	return false;
+{	
+	if
+	(
+		r_x1 == expected_r_x1 && i_x1 == expected_i_x1 &&
+		r_x2 == expected_r_x2 && i_x2 == expected_i_x2		
+	) 
+	{return true;}
+	
+	return false;	
 }
 
 int 
 main()                 
 {      
-	double test[NUMBER_OF_TESTS][6] =
+	double test[NUMBER_OF_TESTS][7] =
 	{ 
-		{1, -4, 4, 2, 2, true}, 
-		{1, 3, -4, 1, -4, false}, 
-		{1, -5, 9, 0, 0, true}
+		{1, -4, 4, 2, 0, 2, 0}, 
+		{1, 3, -4, 1, 0, -4, 0}, 
+		{1, -2, 10, 1, 3, 1, -3}
 	};
-	double failed_tests[NUMBER_OF_TESTS][4];
+	double failed_tests[NUMBER_OF_TESTS][5];
 	int number_of_failed_tests;
 	
 	double *x;
-	bool is_solution_found;
-	
+		
 	printf("Testing has started...\n");
 	
-	x = malloc(2 * sizeof(double));
+	x = malloc(4 * sizeof(double));
 	number_of_failed_tests = 0;
 	for(int i = 0; i < NUMBER_OF_TESTS; i++)
 	{
 		printf("\rCurrent test: %d", i + 1);
 	
-		is_solution_found = solve_equation(test[i][0], test[i][1], test[i][2], x);
+		solve_equation(test[i][0], test[i][1], test[i][2], x);
 		if
 		(
 			!(check_the_solution
 			(
-				x[0], x[1], is_solution_found,
-				test[i][3], test[i][4], test[i][5]
+				x[0], x[1], x[2], x[3],
+				test[i][3], test[i][4], test[i][5], test[i][6]
 			))
 		)
 		{
 			failed_tests[number_of_failed_tests][0] = x[0];
 			failed_tests[number_of_failed_tests][1] = x[1];
-			failed_tests[number_of_failed_tests][2] = is_solution_found;
-			failed_tests[number_of_failed_tests][3] = i;
+			failed_tests[number_of_failed_tests][2] = x[2];
+			failed_tests[number_of_failed_tests][3] = x[3];
+			failed_tests[number_of_failed_tests][4] = i;
 			number_of_failed_tests++;
 		}
 	}	
@@ -85,12 +86,12 @@ main()
 	printf("Failed tests:");
 	for(int i = 0; i < number_of_failed_tests; i++)
 	{
-		int k = failed_tests[i][3];
+		int k = failed_tests[i][4];
 		output_solution
 		( 
-  			failed_tests[i][0], failed_tests[i][1], failed_tests[i][2],
+  			failed_tests[i][0], failed_tests[i][1], failed_tests[i][2], failed_tests[i][3],
   			test[k][0], test[k][1], test[k][2], 
-  			test[k][3], test[k][4], test[k][5]
+  			test[k][3], test[k][4], test[k][5], test[k][6]
   		);
 	}
 	
