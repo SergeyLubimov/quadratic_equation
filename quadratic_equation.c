a44c11011e07e0e874fe4c754a3310be4f8be1ca
SergeyLubimov <logoskaizar@yandex.ru>
2024-05-06 15:15:17 +0700

    Expand the test suite and fix the error at a = 0.

--- quadratic_equation.c
+++ quadratic_equation.c
@@ -5,9 +5,10 @@ bool solve_equation(double a, double b, double c, double *x)
 	double D = b * b - 4 * a * c;
 	
 	D = sqrt(D);
-	a *= 2;
-	if(D >= 0)
-	{		
+	
+	if(D >= 0 && a != 0)
+	{	
+		a *= 2;	
 		x[0] = (-b + D) / a;
 		x[1] = (-b - D) / a;
 		return true;
--- set_of_tests.h
+++ set_of_tests.h
@@ -1,9 +1,14 @@
-#define NUMBER_OF_TESTS 3
+#define NUMBER_OF_TESTS 7
 
 #define SET_OF_TESTS  				\
 { 						\
 		{1, -4, 4, 2, 2, true}, 	\
 		{1, 3, -4, 1, -4, true}, 	\
-		{1, -5, 9, 0, 0, false}		\
-}						\
+		{1, -5, 9, 0, 0, false},	\
+		{4, 0, -16, 2, -2, true},	\
+		{-4, 0, 16, -2, 2, true},	\
+		{0, 0, 0, 0, 0, false},		\
+		{4, 0, 16, 0, 0, false}	  	\
+}
+
 
